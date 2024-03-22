0bfb57b7f2de2222cc9e3c4d87f23043541b7f63
CamThuy02 <ntct0212@gmail.com>
2024-03-22 09:51:51 +0700

    Test font

---
+++ App.js
@@ -0,0 +1,23 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text style={{ fontSize: 20, fontFamily:'DancingScript-Regular'
+      }}>Custom Font</Text>
+      <Text style={{ fontSize: 20, fontFamily:'Poppins-Regular'
+      }}>Custom Font</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
